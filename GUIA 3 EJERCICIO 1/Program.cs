d6de87f9b65ec1e7badb2adeed9df39492aea167
Rebe-Alvarenga <rebequitasarai1@gmail.com>
2021-01-29 18:32:14 -0600

    Start Project

---
+++ GUIA 3 EJERCICIO 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GUIA_3_EJERCICIO_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
